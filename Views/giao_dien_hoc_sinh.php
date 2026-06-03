12d91f1a13e5f8109262e7d449945378c4478b46
24520353 <24520353@gm.uit.edu.vn>
2026-06-03 20:06:42 +0700

    style: update giao diện btn đăng tin

--- Views/giao_dien_hoc_sinh.php
+++ Views/giao_dien_hoc_sinh.php
@@ -249,15 +249,16 @@ require_once __DIR__ . '/partials/header.php';
     </div>
 </div>
 
-        <button type="button" class="btn btn-gocgiasu rounded-pill shadow-lg d-flex align-items-center p-2 pe-3 fab-custom"
-            data-bs-toggle="modal" 
+        <button
+            type="button"
+            class="btn btn-gocgiasu fab-custom shadow-lg"
+            data-bs-toggle="modal"
             data-bs-target="#modalDangTin">
-            <div class="bg-white rounded-circle d-flex align-items-center justify-content-center me-2" style="width: 32px; height: 32px;">
-                <i class="bi bi-plus text-teal fw-bold" style="font-size: 1.5rem; line-height: 0;"></i>
-            </div>
-            <span class="text-navy fw-bold" style="font-size: 1.2rem;">Đăng tin tìm gia sư</span>
-        </button>
 
+            <i class="bi bi-megaphone-fill"></i>
+            <span>Đăng tin tìm gia sư</span>
+
+        </button>
 
         <div class="modal fade" id="modalDangTin" tabindex="-1" aria-hidden="true">
             <div class="modal-dialog modal-dialog-centered" style="max-width: 450px;">
